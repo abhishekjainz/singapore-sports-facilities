70a86e44427f30099e2c6be22a3c30321f3c581c
kian <65789711+kianlin@users.noreply.github.com>
2022-11-10 00:32:54 +0800

    coordinates converter from SVY21 to WGS84

---
+++ Coordinates_Converter.py
@@ -0,0 +1,40 @@
+import json
+import requests
+import pandas as pd
+
+website = "https://developers.onemap.sg"
+
+################################
+##### COORDINATE CONVERTER #####
+################################
+
+root = "data/dus_school_sports_facilities/"
+
+school_list = pd.read_csv(root + "dus_school_sports_facilities.csv")
+
+nrow = len(school_list)
+school_list["Longitude"] = 0
+school_list["Latitude"] = 0
+
+# Loop through the Onemap API to convert all X-Y coordinates to Lat/Long values
+for i in range(nrow):
+    X_coord = school_list["X_ADDR"][i]
+    Y_coord = school_list["Y_ADDR"][i]
+    
+    # GET request for coordinates information, and read into the JSON output
+    query = website + f"/commonapi/convert/3414to4326?X={X_coord}&Y={Y_coord}"
+    getLatLong = requests.get(query)
+    data = json.loads(getLatLong.content)
+    
+    try:
+        school_list["Longitude"][i] = data['longitude']
+        school_list["Latitude"][i] = data['latitude']
+    except:
+        print("ERORR for %d" % (i))
+        pass
+
+print("Completed!")
+
+# Convert Dataframe to CSV format
+school_list.to_csv("data/dus_school_sports_facilities/" + 'dus_school_sports_facilities_updated.csv', index = False)
+
